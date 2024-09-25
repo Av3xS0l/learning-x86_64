6c319a986a90a6a8b82a0d430a45a58c7593ae6a
Av3xS0l <pgustavsr@gmail.com>
2024-09-25 13:16:32 +0300

    finished the file

--- Chapt22/adouble.asm
+++ Chapt22/adouble.asm
@@ -1,1 +1,21 @@
 ; adouble.asm
+section .data
+section .bss
+section .text
+    global  adouble
+adouble:
+    section .text
+        push    rbp
+        mov     rbp, rsp
+        mov     rcx, rsi
+        mov     rbx, rdi
+        xor     r12, r12
+        dloop:
+            movsd   xmm0, qword [rbx+r12*8]
+            addsd   xmm0, xmm0
+            movsd   qword [rbx+r12*8], xmm0
+            inc     r12
+            cmp     r12, rcx
+            jl      dloop
+        leave
+        ret
